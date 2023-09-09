39ab13835c62035c3bb088fc8e751f9de5cdd20c
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2023-09-09 14:34:35 +0200

    :wrench: using multiple submodules from main file.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <submodule/print_to_console.hpp>
+#include <print_to_console_from_2ndsubmodule.hpp>
 
 int main(int argc, const char** argv) {
   
   submodule::print_to_console();
+  a_2nd_submodule::print_to_console();
 
   return 0;
 } 
