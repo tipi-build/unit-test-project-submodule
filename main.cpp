62e796ed8f5e5f3d4a486faf9e5e9261ba9d48e1
Lambourl <65184664+Lambourl@users.noreply.github.com>
2023-11-09 18:02:35 +0100

    :wrench: add get_version

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <submodule/print_to_console.hpp>
+#include <submodule/version.hpp>
 
 int main(int argc, const char** argv) {
   
   submodule::print_to_console();
+  std::cout<<get_version()<<std::endl;
 
   return 0;
 } 
